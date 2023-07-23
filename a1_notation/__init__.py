47f796179dc43ad9b439cba61ac5dea21e4587dc
brooketalcott <brooketalcott@users.noreply.github.com>
2023-07-22 18:09:48 -0600

    Fix __all__ str

--- a1_notation/__init__.py
+++ a1_notation/__init__.py
@@ -5,4 +5,4 @@ A1 Notation lookups in various formats for pre-computed or generated-as-needed u
 from . import column_lookup
 from . import exceptions
 
-__all__ = (column_lookup, exceptions)
+__all__ = ("column_lookup", "exceptions")
